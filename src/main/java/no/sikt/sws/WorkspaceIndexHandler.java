654f807f5ef3719abf00da7846d9b0cda8ae8e35
Marina Atkina <marina.atkina@tietoevry.com>
2022-09-08 15:16:21 +0200

    NP-12507: small change to check if Request Handler will work

--- src/main/java/no/sikt/sws/WorkspaceIndexHandler.java
+++ src/main/java/no/sikt/sws/WorkspaceIndexHandler.java
@@ -1,12 +1,14 @@
 package no.sikt.sws;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.fasterxml.jackson.databind.util.JSONWrappedObject;
 import no.sikt.sws.exception.SearchException;
 import nva.commons.apigateway.ApiGatewayHandler;
 import nva.commons.apigateway.RequestInfo;
 import nva.commons.apigateway.exceptions.ApiGatewayException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.json.JSONObject;
 
 import static no.sikt.sws.constants.ApplicationConstants.OPENSEARCH_ENDPOINT_ADDRESS;
 
@@ -37,8 +39,8 @@ public class WorkspaceIndexHandler extends ApiGatewayHandler<Void, IndexResponse
             var response = restClientOpenSearch.sendRequest(httpMethod, url);
             logger.info("response-code:" + response.getStatus());
             logger.info("response-body:" + response.getBody());
-
-            return new IndexResponse(response.getBody());
+            var jsonResult = new JSONObject(response.getBody());
+            return new IndexResponse(jsonResult);
         } catch (Exception e) {
             logger.error("Error when communicating with opensearch:" + e.getMessage(), e);
             throw new SearchException(e.getMessage(), e);
