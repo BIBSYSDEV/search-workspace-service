a1d502a6dfe5f79ea21f779ba8ebee6baf4a21b0
Sondre Vestad <sondre.vestad@tietoevry.com>
2022-09-16 22:11:44 +0200

    fix scopes

--- src/test/java/no/sikt/sws/testutils/TestConstants.java
+++ src/test/java/no/sikt/sws/testutils/TestConstants.java
@@ -2,7 +2,7 @@ package no.sikt.sws.testutils;
 
 public final class TestConstants {
 
-    public static final String TEST_SCOPE  = "https://api.sws.sikt.no/scopes/workspace-sondre";
+    public static final String TEST_SCOPE  = "https://api.sws.aws.sikt.no/scopes/workspace-sondre";
     public static final String TEST_INDEX_1  = "some-index-1";
     public static final String TEST_WORKSPACE_PREFIX = "workspace-sondre-";
 }
