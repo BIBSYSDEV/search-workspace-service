cd96cc61a0fada2530267bbcaef28041882c9792
Sondre Vestad <sondre.vestad@tietoevry.com>
2022-09-19 15:25:19 +0200

    fixed requestutil to get workspace from claim-list

--- src/main/java/no/sikt/sws/RequestUtil.java
+++ src/main/java/no/sikt/sws/RequestUtil.java
@@ -3,9 +3,7 @@ package no.sikt.sws;
 import com.amazonaws.http.HttpMethodName;
 import nva.commons.apigateway.RequestInfo;
 import nva.commons.core.JacocoGenerated;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.util.Arrays;
 import static no.sikt.sws.constants.ApplicationConstants.SCOPE_IDENTIFIER;
 import static nva.commons.apigateway.RequestInfoConstants.SCOPES_CLAIM;
 import static nva.commons.core.attempt.Try.attempt;
@@ -14,8 +12,6 @@ import static nva.commons.core.attempt.Try.attempt;
 public class RequestUtil {
 
     public static final String HTTP_METHOD = "httpMethod";
-
-    private static final Logger logger = LoggerFactory.getLogger(RequestUtil.class);
 
     public static HttpMethodName getRequestHttpMethod(RequestInfo requestInfo) {
         return attempt(() ->
@@ -28,10 +24,14 @@ public class RequestUtil {
 
     public static String getWorkspace(RequestInfo request) {
         return attempt(() -> {
-            var fullScope = request.getRequestContextParameter(SCOPES_CLAIM);
-            logger.info("fullScope:" + fullScope);
+            var claims = request.getRequestContextParameter(SCOPES_CLAIM).split(" ");
+            var specificWorkspaceClaim = Arrays
+                    .stream(claims)
+                    .filter(c -> !c.equals(SCOPE_IDENTIFIER + "/workspace"))
+                    .findFirst()
+                    .orElseThrow();
 
-            return fullScope.replaceFirst(SCOPE_IDENTIFIER + "/", "");
+            return specificWorkspaceClaim.replaceFirst(SCOPE_IDENTIFIER + "/", "");
         }).orElseThrow();
     }
 }
