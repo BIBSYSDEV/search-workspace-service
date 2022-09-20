80e9882909ada4add91836b7c256ab5029d0a58a
Fredrik Pfeil <57641333+fredrikpf@users.noreply.github.com>
2022-09-20 14:17:21 +0200

    added link to WorkspaceResponse

--- src/test/java/no/sikt/sws/WorkspaceResponseTest.java
+++ src/test/java/no/sikt/sws/WorkspaceResponseTest.java
@@ -2,25 +2,37 @@ package no.sikt.sws;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import nva.commons.core.Environment;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsEqual.equalTo;
+import static no.sikt.sws.constants.ApplicationConstants.API_GATEWAY_URL;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class WorkspaceResponseTest {
 
+    ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     void workspaceResponseMapsCorrectly() throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
 
         String mockJson =
                 "{\"account_identifier\":\"hei\",\"index_list\":{\"hallo\":{\"hade\":\"somevalue\"}}}";
 
         WorkspaceResponse workspaceObject = objectMapper.readValue(mockJson, WorkspaceResponse.class);
-        String mappedString = objectMapper.valueToTree(workspaceObject).toString();
-        assertThat(mappedString, is(equalTo(mockJson)));
+
+        assertEquals("hei", workspaceObject.accountIdentifier);
+        assertEquals("{\"hallo\":{\"hade\":\"somevalue\"}}", workspaceObject.indexList.toString());
+        assertNull(workspaceObject.createIndexLink);
+    }
+
+    @Test
+    void addHypperLinkToWorkspaceResponseIfEmptyIndexList() throws JsonProcessingException {
+        var workspaceResponse = WorkspaceResponse.fromValues("mockWorkSpace", "");
+
+        assertEquals("", workspaceResponse.indexList.toString());
+
+        assertEquals(API_GATEWAY_URL + "/index_name", workspaceResponse.createIndexLink);
     }
 
 
