ecc62fc8d60b66c35ca08e17267ce36b4aee0a23
Sondre Vestad <sondre.vestad@tietoevry.com>
2022-09-20 12:32:43 +0200

    added optional error passthough

--- src/main/java/no/sikt/sws/IndexHandler.java
+++ src/main/java/no/sikt/sws/IndexHandler.java
@@ -2,20 +2,20 @@ package no.sikt.sws;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import no.sikt.sws.exception.SearchException;
+import nva.commons.apigateway.ApiGatewayProxyHandler;
+import nva.commons.apigateway.ProxyResponse;
 import nva.commons.apigateway.RequestInfo;
 import nva.commons.apigateway.exceptions.ApiGatewayException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import software.amazon.awssdk.utils.Pair;
-
 
 /**
  * Created for checking if external libraries have been imported properly.
  */
-public class IndexHandler extends OpenSearchProxyHandler<String, String> {
+public class IndexHandler extends ApiGatewayProxyHandler<String, String> {
 
     public static final String RESOURCE_IDENTIFIER = "resource";
-    public OpenSearchClient openSearchClient = new OpenSearchClient();
+    public OpenSearchClient openSearchClient = OpenSearchClient.passthroughClient();
 
 
     private static final Logger logger = LoggerFactory.getLogger(IndexHandler.class);
@@ -25,7 +25,7 @@ public class IndexHandler extends OpenSearchProxyHandler<String, String> {
     }
 
     @Override
-    Pair<String, Integer> inputToOutputWithStatus(String body, RequestInfo request, Context context)
+    protected ProxyResponse<String> processProxyInput(String body, RequestInfo request, Context context)
             throws ApiGatewayException {
         var httpMethod = RequestUtil.getRequestHttpMethod(request);
         var workspace = RequestUtil.getWorkspace(request);
@@ -35,25 +35,18 @@ public class IndexHandler extends OpenSearchProxyHandler<String, String> {
             var url = workspace + "-" + resourceIdentifier;
             logger.info("URL: " + url);
 
-            var response = openSearchClient.sendRequest(
-                    httpMethod,
-                    url,
-                    body);
-            //out of scope
-            //WorkspaceStripper.add(body, workspace, resourceIdentifier));
-            logger.info("response-code:" + response.getStatus());
-            logger.info("response-body:" + response.getBody());
+            var response = openSearchClient.sendRequest(httpMethod, url, body);
 
+            logger.info("response-code:" + response.getStatus());
+            logger.info("raw response-body:" + response.getBody());
 
             var responseBody = WorkspaceStripper.remove(response.getBody(), workspace);
-            logger.info("response-body:" + responseBody);
+            logger.info("stripped response-body:" + responseBody);
 
-            return Pair.of(responseBody, response.getStatus());
+            return new ProxyResponse<>(response.getStatus(), responseBody);
         } catch (Exception e) {
             logger.error("Error when communicating with opensearch:" + e.getMessage(), e);
             throw new SearchException(e.getMessage(), e);
         }
     }
-
-
 }
