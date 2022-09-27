54c53b8cc7106e5556c19a57e9d567307944a99a
Marina Atkina <marina.atkina@tietoevry.com>
2022-09-27 11:27:20 +0200

    Small mistake and orthography errors fixes

--- src/main/java/no/sikt/sws/RegisterSnapshotRepoS3.java
+++ src/main/java/no/sikt/sws/RegisterSnapshotRepoS3.java
@@ -6,10 +6,7 @@ import no.sikt.sws.exception.SearchException;
 import org.json.JSONObject;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 
-;
 import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,31 +19,27 @@ public class RegisterSnapshotRepoS3 {
     ProfileCredentialsProvider credentialsProvider =
             ProfileCredentialsProvider.create();
     Region region = Region.EU_WEST_1;
-    private String bucketName = BACKUP_BUCKET_NAME;
-    S3Client s3 = S3Client.builder()
-            .region(region)
-            .credentialsProvider(credentialsProvider)
-            .build();
+    private final String bucketName = BACKUP_BUCKET_NAME;
+
 
     public OpenSearchClient openSearchClient = OpenSearchClient.passthroughClient();
 
 
 
-    public OpenSearchResponse registerRepository() throws SearchException{
+    public OpenSearchResponse registerRepository() throws SearchException {
         JSONObject jsonSettings = new JSONObject();
         JSONObject jsonRepo = new JSONObject();
         jsonRepo.put("type", "s3");
-        jsonSettings.put( "bucket", "");
-        jsonSettings.put( "base_path","/snapshots");
+        jsonSettings.put("bucket", bucketName);
+        jsonSettings.put("base_path","/snapshots");
         jsonRepo.put("settings", jsonSettings);
         String jsonRequest = jsonRepo.toString();
 
         try {
-        var response = openSearchClient.sendRequest(HttpMethodName.PUT, "", jsonRequest);
-        logger.info("response-code:" + response.getStatus());
-        return response;
-    }
-        catch(Exception e){
+            var response = openSearchClient.sendRequest(HttpMethodName.PUT, "", jsonRequest);
+            logger.info("response-code:" + response.getStatus());
+            return response;
+        } catch (Exception e) {
             logger.error("Error when attempting to set snapshot repository:" + e.getMessage(), e);
             throw new SearchException(e.getMessage(), e);
         }
