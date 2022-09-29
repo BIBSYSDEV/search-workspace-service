e9c54c65eb1c3f7b91a30ce1c64c438344a7e3c4
Stig Norland <stig.norland@sikt.no>
2022-09-29 10:51:55 +0200

    fixed const

--- src/main/java/no/sikt/sws/WorkspaceStripper.java
+++ src/main/java/no/sikt/sws/WorkspaceStripper.java
@@ -15,6 +15,7 @@ import static nva.commons.core.attempt.Try.attempt;
 public class WorkspaceStripper {
 
     private static final Logger logger = LoggerFactory.getLogger(WorkspaceStripper.class);
+    public static final String INDEX = "_index";
 
     // Remove {workspace}- from responseBody but only if beginning of field, word og preceded by '/'
     public static String remove(String body, String workspace) {
@@ -58,13 +59,13 @@ public class WorkspaceStripper {
         return gatewayBody.stream().map(item -> {
             String indexName;
             if (item.has("index")) {
-                indexName = item.get("index").get("_index").textValue();
+                indexName = item.get("index").get(INDEX).textValue();
             } else if (item.has("delete")) {
-                indexName = item.get("delete").get("_index").textValue();
+                indexName = item.get("delete").get(INDEX).textValue();
             } else if (item.has("update")) {
-                indexName = item.get("update").get("_index").textValue();
+                indexName = item.get("update").get(INDEX).textValue();
             } else if (item.has("create")) {
-                indexName = item.get("create").get("_index").textValue();
+                indexName = item.get("create").get(INDEX).textValue();
             } else {
                 return item.toString();
             }
