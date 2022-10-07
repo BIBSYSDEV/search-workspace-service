1f077218d42d4e00284cb41572483d3da92e7f2a
Fredrik Pfeil <57641333+fredrikpf@users.noreply.github.com>
2022-10-07 11:30:06 +0200

    set max result for listResourceServersRequest to 50

--- src/main/java/no/sikt/sws/CognitoHandler.java
+++ src/main/java/no/sikt/sws/CognitoHandler.java
@@ -81,7 +81,7 @@ public class CognitoHandler extends ApiGatewayHandler<Void, Void> {
         var listResourceServersRequest = ListResourceServersRequest
                 .builder()
                 .userPoolId(userPoolId)
-                .maxResults(100)
+                .maxResults(50)
                 .build();
         var resources = cognitoClient.listResourceServers(listResourceServersRequest);
 
