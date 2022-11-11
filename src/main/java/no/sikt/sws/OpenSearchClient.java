32163fda84bd43ab48216c0ba38a25b1fbfc5816
Marina Atkina <marina.atkina@tietoevry.com>
2022-11-11 09:36:20 +0100

    Fix for special characters

--- src/main/java/no/sikt/sws/OpenSearchClient.java
+++ src/main/java/no/sikt/sws/OpenSearchClient.java
@@ -51,15 +51,17 @@ public class OpenSearchClient {
     }
 
     private Request<Void> buildRequest(HttpMethodName httpMethod, String path, String data) {
-        var pathWithoutParams = stripParametersFromPath(path).toString();
-        var endpoint = buildUri(pathWithoutParams);
-        var query = buildUri(path).getQuery();
+
+        var splitPath = path.split("\\?", 2);
+        var endpoint = splitPath[0];
+        var query = splitPath.length > 1 ? splitPath[1] : null;
+
 
         logger.info(endpoint + " - " + query);
 
         Request<Void> request = new DefaultRequest<>("es");
         request.setHttpMethod(httpMethod);
-        request.setEndpoint(endpoint);
+        request.setEndpoint(buildUri(endpoint));
 
         if (query != null) {
             request.setParameters(getStringListMap(query));
