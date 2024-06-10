28b549f6f47aea498d4302fbfae903d2f44a0a1a
Sondre Vestad <sonves@gmail.com>
2024-06-10 10:32:31 +0200

    set sort as object

--- src/main/java/no/sikt/sws/models/opensearch/DocDto.java
+++ src/main/java/no/sikt/sws/models/opensearch/DocDto.java
@@ -48,8 +48,6 @@ public class DocDto extends Dto {
     @JsonProperty("_source")
     public JsonNode source;
 
-    //@JsonDeserialize(using = ExampleDeserializer.class)
-    //@JsonSerialize(using = CustomDateSerializer.class )
     @JsonProperty("sort")
     public List<Object> sort;
 
