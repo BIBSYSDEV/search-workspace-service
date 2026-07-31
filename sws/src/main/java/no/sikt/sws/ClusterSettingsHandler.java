002d2ae5976dfd587a728da70059e3bbb2f428cd
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2026-07-31 12:47:28 +0200

    [np-51495] feat: Add configuration handler for cluster settings (#119)
    Ticket: https://sikt.atlassian.net/browse/NP-51495
    
    The main purpose here is to configure our OpenSearch clusters with
    `action.auto_create_index: false` to eliminate recurring issues with
    automatically created indices with incorrect mappings. The new
    `ClusterSettingsHandler` is intended to be triggered both automatically
    on every deploy (by listening to CloudFormation events) and as a
    scheduled weekly job.
    
    Changes:
    
    - [feat: Add
    ClusterSettingsHandler](https://github.com/BIBSYSDEV/search-workspace-service/pull/119/commits/4831786038a8c50ac424369f169be00c9e3a11aa)
    - [build: Upgrade
    dependencies](https://github.com/BIBSYSDEV/search-workspace-service/pull/119/commits/81c161084f00d834431d448b9ba5d52c0e2760a8)
    - [build: Upgrade Gradle to
    9.6.1](https://github.com/BIBSYSDEV/search-workspace-service/pull/119/commits/2df52c3c2abbb4f53be2269f7f9c1614fea02d5d)

---
+++ sws/src/main/java/no/sikt/sws/ClusterSettingsHandler.java
@@ -0,0 +1,44 @@
+package no.sikt.sws;
+
+import static java.net.HttpURLConnection.HTTP_OK;
+
+import com.amazonaws.http.HttpMethodName;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import java.nio.file.Path;
+import nva.commons.core.JacocoGenerated;
+import nva.commons.core.ioutils.IoUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ClusterSettingsHandler implements RequestHandler<Object, String> {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(ClusterSettingsHandler.class);
+  private static final String CLUSTER_SETTINGS_PATH = "_cluster/settings";
+  private static final String CLUSTER_SETTINGS =
+      IoUtils.stringFromResources(Path.of("cluster_settings.json"));
+  private final OpenSearchClient openSearchClient;
+
+  @JacocoGenerated
+  public ClusterSettingsHandler() {
+    this(OpenSearchClient.defaultClient());
+  }
+
+  public ClusterSettingsHandler(OpenSearchClient openSearchClient) {
+    this.openSearchClient = openSearchClient;
+  }
+
+  @Override
+  public String handleRequest(Object input, Context context) {
+    LOGGER.info("Applying cluster settings: {}", CLUSTER_SETTINGS);
+    var response =
+        openSearchClient.sendRequest(HttpMethodName.PUT, CLUSTER_SETTINGS_PATH, CLUSTER_SETTINGS);
+
+    if (response.getStatus() != HTTP_OK) {
+      LOGGER.error("Failed to apply cluster settings: {}", response);
+      throw new IllegalStateException("Failed to apply cluster settings");
+    }
+    LOGGER.info("Successfully applied cluster settings: {}", response);
+    return response.getBody();
+  }
+}
